b89dbad1a04bd03acdd1ccfbbda472c33636ab13
bjlagman12 <bjlagman@gmail.com>
2019-02-24 13:32:47 -0800

    Create config file to db login

---
+++ server/dbs/config.file.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  config: {
+    username: 'ENTER YOUR POSTGRESQL USERNAME',
+    password: 'ENTER YOUR POSTGRESQL PASSWORD IF NO PASSWORD TYPE NULL (NOT IN A STRING)',
+  },
+};
