97cebcce13b1b1aac3957cfed7820602bf4173c1
Brian Lagman <36527396+bjlagman12@users.noreply.github.com>
2019-02-25 13:40:19 -0800

    Revert "Data seeding/ api call"

--- server/dbs/config.file.example.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  config: {
-    username: 'ENTER YOUR POSTGRESQL USERNAME',
-    password: 'ENTER YOUR POSTGRESQL PASSWORD IF NO PASSWORD TYPE NULL (NOT IN A STRING)',
-  },
-};
